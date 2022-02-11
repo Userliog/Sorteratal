4d433c06eff95dd530bd9b0dd34f33de13ae75c9
Userliog <92377001+Userliog@users.noreply.github.com>
2022-02-11 15:44:17 +0100

    Add files via upload

---
+++ Insertionsort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Bubble_sort
+{
+    class Program
+    {
+        public static void Swap(ref int a, ref int b){
+            int Temp=a;
+            a=b;
+            b=Temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int length = 1000;
+            Console.WriteLine("insertion sort w. "+length);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            List<int> A = new List<int>();
+            
+            int n =length, j, val, flag;
+            
+            for (int s = 0; s < length; s++)
+            {
+               Random rnd = new Random();
+                A.Add(rnd.Next()); 
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                val=A[i];
+                flag=0;
+                for (j =i-1;j >= 0 && flag != 1; )
+                {
+                    if (val < A[j])
+                    {
+                        A[j+1] = A[j];
+                        j--;
+                        A[j+1] = val;
+                    }
+                    else flag =1;
+                }
+            }
+            
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds+" ms");
+        }
+    }
+}
---
+++ Mergesort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Mergesort
+{
+    class Program
+    {
+        static public void Merge(List A, int b, int c, int d)
+        {
+            int y, z;
+            int l1 = c-b+1;
+            int r1 = d-b;
+            List[] L = new List[l1];
+            List[] R = new List[r1];
+            for (int i=0; y<r1; y++)
+            {
+                L[i] = A[b+i];
+            }
+            for (y = 0; y < r1; y++)
+            {
+                R[y] = A[c+1+y];
+            }
+        }
+        static public void Merge_sort(List A, int b, int c)
+        {
+            if (b<c)
+            {
+                int d= (b+c)/2;
+                Merge_sort(A, b, d);
+                Merge_sort(A, d+1, c);
+                Merge(A, b, d, c);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int length = 1000;
+            Console.WriteLine("Merge sort w. "+length);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            List<int> A = new List<int>();
+            int n =length;
+            
+            for (int s = 0; s < length; s++)
+            {
+               Random rnd = new Random();
+                A.Add(rnd.Next()); 
+            }
+
+
+
+            foreach (int b in A)
+            {
+                Console.WriteLine(b);   
+            }
+           
+        }
+    }
+}
